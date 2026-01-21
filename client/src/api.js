cc0e646a895832cb72df1028015660627d7cc625
Mohammed Salim <simohammedsalim3@gmail.com>
2026-01-21 21:43:27 +0100

    Add debug log

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+console.log("DEBUG: VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
+
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL
+    ? `${import.meta.env.VITE_API_BASE_URL}/api`
+    : "http://localhost:5000/api",
 });
