367361dc989cac4c927071dd6dad66e97a043447
Mohammed Salim <simohammedsalim3@gmail.com>
2026-01-21 21:57:44 +0100

    Fix DB connection port and SSL

--- server/db.js
+++ server/db.js
@@ -7,6 +7,8 @@ export const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : undefined,
 });
 
 db.connect((err) => {
